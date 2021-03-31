af89f26655a84947179d2bc001141063f5586efc
Nerea García <nereagarcia.c@gmail.com>
2021-03-31 13:32:59 +0200

    reorganizar preguntas en 1 prompt + when

--- index.js
+++ index.js
@@ -8,23 +8,48 @@ inquirer
       name: "transporte",
       type: "list",
       message: "¿Qué tipo de transporte quiere consultar?",
-      choices: ["bus", "metro"]
+      choices: ["bus", "metro"],
+    },
+    {
+      name: "metro",
+      type: "checkbox",
+      message: "¿Qué información extra quiere obtener de cada parada?",
+      choices: ["Coordenadas", "Fecha de inauguración"],
+      when: eleccion => eleccion.transporte === "metro"
+    },
+    {
+      name: "info",
+      type: "confirm",
+      message: "¿Quiere que le informemos de los errores?",
+      when: eleccion => eleccion.metro !== []
+    },
+    {
+      name: "linea",
+      type: "input",
+      message: "¿Qué línea quiere consultar?",
+      when: eleccion => eleccion.info !== ""
     }
-  ])
-  .then(respuesta => {
+  ]);
+
+/* .then(respuesta => {
     if (respuesta.transporte === "bus") {
       console.log(chalk.yellow("No tenemos información dispoinible sobre los buses +info: https://www.tmb.cat/es/home"));
       process.exit(0);
     }
     if (respuesta.transporte === "metro") {
-      inquirer.prompt([{
+      [{
         name: "metro",
         type: "checkbox",
         message: "¿Qué información extra quiere obtener de cada parada?",
-        choices: ["Coordenadas", "Fecha de inauguración"]
-      }]);
-    }
-  });
+        choices: ["Coordenadas", "Fecha de inauguración"],
+        when: respuesta => respuesta === "true"
+      }],
+      {
+        name: "info",
+        type: "confirm",
+        message: "¿Quiere que le informemos de los errores?",
+      }
+    }); */
 
 /* program
   .option("-t, --transporte <transporte>", "¿Qué tipo de transporte quiere consultar?");
