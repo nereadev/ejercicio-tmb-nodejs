160857bfcb10f915a0ea16baf8257c3363fa391d
Nerea García <nereagarcia.c@gmail.com>
2021-03-31 19:07:53 +0200

    importar paradas.json + respuesta si existo o no

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const inquirer = require("inquirer");
 const chalk = require("chalk");
 const fetch = require("node-fetch");
 const arrayPreguntas = require("./arrayPreguntas.js");
+const paradas = require("./paradas.json");
+
+const paradaSeleccionada = paradas.data.ibus;
+console.log(paradaSeleccionada.line);
 
 inquirer
   .prompt(arrayPreguntas)
@@ -11,8 +15,15 @@ inquirer
       console.log(chalk.yellow("No tenemos información dispoinible sobre los buses +info: https://www.tmb.cat/es/home"));
       process.exit(0);
     }
-    const informacionUsuario = respuesta;
-    return informacionUsuario;
+    if (respuesta.linea === paradaSeleccionada.line) {
+      console.log(`
+        Línea: ${paradaSeleccionada.line}.
+        Destino: ${paradaSeleccionada.destination}.
+        Tiempo de espera: ${paradaSeleccionada["t-in-min"]} min.`);
+    } else if (respuesta.linea !== paradaSeleccionada.line && respuesta.info) {
+      console.log(chalk.red.bold("No existe la línea."));
+      process.exit(0);
+    }
   });
 
 program
