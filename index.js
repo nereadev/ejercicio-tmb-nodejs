8724220567ea2e0a51159bb29f92b8dcf8f4a326
Nerea García <nereagarcia.c@gmail.com>
2021-03-31 12:28:46 +0200

    pregunta transporte

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const { program } = require("commander");
 const inquirer = require("inquirer");
 
-program
+inquirer
+  .prompt([
+    {
+      name: "transporte",
+      type: "string",
+      message: "Qué tipo de transporte quiere consultar?"
+    }
+  ])
+  .then(respuesta => { console.log(respuesta); });
+
+/* program
   .option("-t, --transporte <transporte>", "¿Qué tipo de transporte quiere consultar?");
 
 program.parse(process.argv);
@@ -11,3 +21,4 @@ const options = program.opts();
 if (options.transporte) {
   console.log("hola");
 }
+ */
